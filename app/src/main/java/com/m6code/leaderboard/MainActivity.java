3e9146150f7d597fdc98c92d3da80a08066affe1
M6code <benjamynfrank101@gmail.com>
2020-08-31 09:43:38 +0100

    Add Tabs, Fragment and ViewPager

--- app/src/main/java/com/m6code/leaderboard/MainActivity.java
+++ app/src/main/java/com/m6code/leaderboard/MainActivity.java
@@ -1,14 +1,36 @@
 package com.m6code.leaderboard;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentActivity;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentPagerAdapter;
+import androidx.viewpager.widget.ViewPager;
+import androidx.viewpager2.widget.ViewPager2;
 
 import android.os.Bundle;
 
+import com.google.android.material.tabs.TabLayout;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class MainActivity extends AppCompatActivity {
+
+    private ViewPager mViewPager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ViewPager mViewPager = (ViewPager) findViewById(R.id.viewPager);
+        SimpleFragmentPagerAdapter adapter = new SimpleFragmentPagerAdapter(this, getSupportFragmentManager());
+        mViewPager.setAdapter(adapter);
+
+        TabLayout tabLayout = (TabLayout) findViewById(R.id.tabLayout);
+        tabLayout.setupWithViewPager(mViewPager);
     }
 }
---
+++ app/src/main/java/com/m6code/leaderboard/SimpleFragmentPagerAdapter.java
@@ -0,0 +1,62 @@
+package com.m6code.leaderboard;
+
+import android.content.Context;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentPagerAdapter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class SimpleFragmentPagerAdapter extends FragmentPagerAdapter {
+
+    private Context mContext; // A variable that holds the name of the current tab
+
+    /**
+     * A simple constructor for the viewpager
+     *
+     * @param context         the current tab on which the viewpager is
+     * @param fragmentManager return the appropriate fragment to the viewpager
+     */
+    public SimpleFragmentPagerAdapter(Context context, FragmentManager fragmentManager) {
+        super(fragmentManager);
+        mContext = context;
+    }
+
+    /**
+     * Get the current position and sets the appropriate fragment
+     *
+     * @param position the current position
+     * @return the appropriate fragment
+     */
+    @Override
+    public Fragment getItem(int position) {
+        if (position == 0) {
+            return new LearningFragment();
+        } else
+            return new SkillFragment();
+    }
+
+    /**
+     * get the number of fragment to be returned
+     *
+     * @return the numbers of fragments
+     */
+    @Override
+    public int getCount() {
+        return 2;
+    }
+
+    @Override
+    public CharSequence getPageTitle(int position) {
+        if (position == 0) {
+            return mContext.getString(R.string.learning_fragment_text);
+//            return "Learning Leaders";
+        } else
+            return mContext.getString(R.string.skill_fragment_text);
+//        return "Skill IQ Leaders";
+    }
+
+}
