c3924dfd45cabb92507888e51fb03747f1fef589
M6code <benjamynfrank101@gmail.com>
2020-09-03 12:28:57 +0100

    Create & Design Submission Form Activity

--- app/src/main/java/com/m6code/leaderboard/MainActivity.java
+++ app/src/main/java/com/m6code/leaderboard/MainActivity.java
@@ -1,5 +1,6 @@
 package com.m6code.leaderboard;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -31,6 +32,7 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View view) {
                 // Show toast here
                 Snackbar.make(view, "Launch Submit Form", Snackbar.LENGTH_LONG).setAction("Action", null).show();
+                startActivity(new Intent(MainActivity.this, SubmitActivity.class));
             }
         });
 
